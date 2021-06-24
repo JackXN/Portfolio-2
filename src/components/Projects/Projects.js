c7be8ee3b1c1094e17a49ce71c3339a2bde016bf
Jack Rigan <riganjack40@gmail.com>
2021-06-23 18:23:50 -0700

    updated projects displats

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,6 +2,7 @@ import React, {useEffect} from 'react'
 import "./Projects.css"
 import "./Projects-2.css"
 
+import {Link} from 'react-router-dom';
 import ProjectImg5 from '../assets/images/grubhubbg.png'
 import BackEndImg from '../assets/images/backend.png'
 import PlaceHold from '../assets/images/placehold.png'
@@ -18,12 +19,73 @@ function Projects() {
 <div className='project-container'>
 <div className='box box-1'>
 <img src={CleanCode}></img>
+<h2>Grub Dash</h2>
+<p>Grub dash is an online delivery service for local restaurants. Grub Dash delivers for free to any place, any time</p>
+<div className='stack-used'>
+<h3>Technologies Used</h3>
+<ul>
+    <li><JavaScriptIcon id ='js'/></li>
+    <li><CssThree id='css'/></li>
+</ul>
+</div>
+
+
+<div className='link-buttons'>
+<ul>
+    <li><Link>Live Demo</Link></li>
+    <li><Link>Github</Link></li>
+</ul>
+
+
+</div>
 </div>
 <div className='box box-1'>
 <img src={CleanCode}></img>
+<h2>PAX SMP</h2>
+<p>PAX SMP is a home directory for a local Minecraft vanilla server. A project i built for some friends who were developing a minecraft server </p>
+<div className='stack-used'>
+<h3>Technologies Used</h3>
+<ul>
+    <li><JavaScriptIcon id ='js'/></li>
+    <li><CssThree id='css'/></li>
+</ul>
+</div>
+
+
+<div className='link-buttons'>
+<ul>
+    <li><Link>Live Demo</Link></li>
+    <li><Link>Github</Link></li>
+</ul>
+
+
+</div>
 </div>
 <div className='box box-1'>
 <img src={CleanCode}></img>
+<h2>FlashCards</h2>
+<p>A flashcard study app that is designed to help quiz developers on the fundamentals of React</p>
+
+<div className='stack-used'>
+<h3>Technologies Used</h3>
+<ul>
+    <li><JavaScriptIcon id ='js'/></li>
+    <li><CssThree id='css'/></li>
+</ul>
+</div>
+
+
+<div className='link-buttons'>
+<ul>
+    <li><Link>Live Demo</Link></li>
+    <li><Link>Github</Link></li>
+</ul>
+
+
+</div>
+
+
+
 </div>
 
 

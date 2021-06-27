30d2b3fe28c0ddaabbb15d8ee03913678b814a20
Jack Rigan <riganjack40@gmail.com>
2021-06-26 23:18:45 -0700

    updated projects section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./Projects.css"
-import "./Projects-2.css"
+import PAX from '../assets/images/pax-img.png'
 
 import {Link} from 'react-router-dom';
 
@@ -10,83 +10,40 @@ function Projects() {
 
     return (
 <section className='projects-section'>
-<h1>Some Of My Work</h1>
-<div className='project-container'>
-<div className='box box-1'>
-<img alt='project-img' src={CleanCode}></img>
-<h2>Grub Dash</h2>
-<p>Grub dash is an online delivery service for local restaurants. Grub Dash delivers for free to any place, any time</p>
-<div className='stack-used'>
-<h3>Technologies Used</h3>
-<ul>
-    <li><JavaScriptIcon id ='js'/></li>
-    <li><CssThree id='css'/></li>
+<div className='projects-container'>
+<h1>Recent Projects</h1>
+<div className='projects-display'>
+<div className='project-1 project'>
+<img className='project-img'src={PAX} alt='paxx'/>
+<div className='project-text'>
+<h3>PAX SMP</h3>
+<h4>[Front-End]</h4>
+<p>A front end project created for a local minecraft survival server. It serves as a main directory for all staff and players</p>
+<ul className='code-used'>
+    <li><JavaScriptIcon/></li>
+    <li><CssThree/></li>
 </ul>
+<button>Launch</button>
+</div>
 </div>
 
-
-<div className='link-buttons'>
-<ul>
-    <li><Link>Live Demo</Link></li>
-    <li><Link>Github</Link></li>
+<div className='project-2 project'>
+<img className='project-img'src={PAX} alt='paxx'/>
+<div className='project-text'>
+<h3>PAX SMP</h3>
+<h4>[Front-End]</h4>
+<p>A front end project created for a local minecraft survival server. It serves as a main directory for all staff and players</p>
+<ul className='code-used'>
+    <li><JavaScriptIcon/></li>
+    <li><CssThree/></li>
 </ul>
-
-
+<button>Launch</button>
 </div>
 </div>
-<div className='box box-1'>
-<img  src={CleanCode} alt='project-img'></img>
-<h2>PAX SMP</h2>
-<p>PAX SMP is a home directory for a local Minecraft vanilla server. A project i built for some friends who were developing a minecraft server </p>
-<div className='stack-used'>
-<h3>Technologies Used</h3>
-<ul>
-    <li><JavaScriptIcon id ='js'/></li>
-    <li><CssThree id='css'/></li>
-</ul>
-</div>
-
-
-<div className='link-buttons'>
-<ul>
-    <li><Link>Live Demo</Link></li>
-    <li><Link>Github</Link></li>
-</ul>
-
-
-</div>
-</div>
-<div className='box box-1'>
-<img src={CleanCode} alt='project-img'></img>
-<h2>FlashCards</h2>
-<p>A flashcard study app that is designed to help quiz developers on the fundamentals of React</p>
-
-<div className='stack-used'>
-<h3>Technologies Used</h3>
-<ul>
-    <li><JavaScriptIcon id ='js'/></li>
-    <li><CssThree id='css'/></li>
-</ul>
-</div>
-
-
-<div className='link-buttons'>
-<ul>
-    <li><Link>Live Demo</Link></li>
-    <li><Link>Github</Link></li>
-</ul>
-
 
 </div>
 
-
-
 </div>
-
-
-
-</div>
-
 
 
 

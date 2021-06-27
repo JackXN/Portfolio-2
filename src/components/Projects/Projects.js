1f44103838c2439de9099a3db489c57dcbd06419
Jack Rigan <riganjack40@gmail.com>
2021-06-27 00:35:13 -0700

    added transitions for projects page

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -10,10 +10,10 @@ function Projects() {
 
     return (
 <section className='projects-section'>
-<h1>Recent Projects</h1>
+<h1 data-aos='fade-down'>Recent Projects</h1>
 <div className='projects-container'>
 <div className='projects-display'>
-<div className='project-1 project'>
+<div data-aos='fade-left'className='project-1 project'>
 <img className='project-img'src={PAX} alt='paxx'/>
 <div className='project-text'>
 <h3>PAX SMP</h3>
@@ -27,10 +27,10 @@ function Projects() {
 </div>
 </div>
 
-<div className='project-2 project'>
+<div  data-aos='fade-right'className='project-2 project'>
 <img className='project-img'src={PAX} alt='paxx'/>
 <div className='project-text'>
-<h3>PAX SMP</h3>
+<h3>Atom</h3>
 <h4>[Front-End]</h4>
 <p>A front end project created for a local minecraft survival server. It serves as a main directory for all staff and players</p>
 <ul className='code-used'>

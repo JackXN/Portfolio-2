eaae877ba9be94399b8f013ed934d0f24741ea25
Jack Rigan <riganjack40@gmail.com>
2021-06-30 00:06:27 -0700

    updated projects ss

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import "./Projects.css"
 import PAX from '../assets/images/pax-img.png'
+import Decoder from '../assets/images/Decoder-ss.png'
+import Lib from '../assets/images/local-lib.png'
 
-
-
-import {SiJavascript as JavaScriptIcon, SiCss3 as CssThree} from 'react-icons/si'
+import {SiJavascript as JavaScriptIcon, SiCss3 as CssThree, SiReact as ReactIcon, SiHtml5} from 'react-icons/si'
 
 function Projects() {
 
@@ -14,33 +14,55 @@ function Projects() {
 <h1 data-aos='fade-down' id='projects'>Recent Projects</h1>
 <div className='projects-display'>
 <div data-aos='fade-left'className='project-1 project'>
-<img className='project-img'src={PAX} alt='paxx'/>
+<img className='project-img'src={Lib} alt='paxx'/>
 <div className='project-text'>
-<h3>Flash-O-Matic</h3>
-<h4>[Front-End]</h4>
-<p>A flashcard study app that can be used to help one study or review important concepts of a learning objective</p>
+<h3>Library Admin</h3>
+<p>A dashboard built for a fictional library that displays records of borrowed and returned books, along with the customers that are associated with those transactions </p>
 <ul className='code-used'>
-    <li><JavaScriptIcon/></li>
-    <li><CssThree/></li>
+    <li><JavaScriptIcon className='javascript'/></li>
+    <li><CssThree className='css'/></li>
+    <li><SiHtml5/></li>
 </ul>
-<button>Launch</button>
+<button><a href='https://jackxn.github.io/Local-Library-Project/'>Launch</a></button>
+<button><a href='https://github.com/JackXN/Local-Library-Project'>Github</a></button>
 </div>
 </div>
+
 
 <div  data-aos='fade-right'className='project-2 project'>
 <img className='project-img'src={PAX} alt='paxx'/>
 
 <div className='project-text'>
-<h3>Weather Zen</h3>
-<h4>[Full-Stack]</h4>
-<p>A fullstack web application that allows users to check the weather forcast</p>
+<h3><span id='p'>PAX</span> SMP</h3>
+
+<p>A front-end freelance project built for a minecraft server. Being used as the main directory for everything related</p>
+<ul className='code-used'>
+    <li className='javascript'><JavaScriptIcon /></li>
+    <li className='css'><CssThree/></li>
+    <li className='react'><ReactIcon/></li>
+</ul>
+<button><a href='https://pax-smp-a3vnz1kgq-jackxn.vercel.app/'>Launch</a></button>
+</div>
+</div>
+
+
+
+
+<div  data-aos='fade-right'className='project-2 project'>
+<img className='project-img'src={Decoder} alt='Decoder'/>
+<div className='project-text'>
+<h3><span id ='d'>Decoder</span> Ring</h3>
+<p>The decoder ring was built to encrypt and decrypt all sorts of secret messages</p>
 <ul className='code-used'>
     <li><JavaScriptIcon/></li>
     <li><CssThree/></li>
 </ul>
-<button>Launch</button>
+<button><a href='https://jackxn.github.io/Decoder-ring-project/'>Launch</a></button>
+<button><a href='https://github.com/JackXN/Decoder-ring-project'>Github</a></button>
 </div>
 </div>
+
+
 
 </div>
 
--- src/components/ResumeBtn/Resume.js
+++ src/components/ResumeBtn/Resume.js
@@ -4,7 +4,7 @@ import './Resume.css'
 function Resume() {
     return (
         <div className='resume-btn'>
-            <button type='button'><a rel="noopener noreferrer" href='https://drive.google.com/file/d/1hvX-AaKlGv8QLUBwm6sn4NUzRDZd8UPq/view?usp=sharing' target='_blank'>Download My Resume!</a></button>
+            <button type='button'><a rel="noopener noreferrer" href='https://drive.google.com/file/d/1gpJFKt2idttsSE9nqSxrqOWzu5B8w0kx/view?usp=sharing' target='_blank'>Download My Resume!</a></button>
 
         </div>
     )

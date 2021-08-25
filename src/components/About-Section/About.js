a42f027e1eb6fded6808732fc650f2c3226f9cf0
Jack Rigan <riganjack40@gmail.com>
2021-08-24 22:27:08 -0600

    updated About me page text

--- src/components/About-Section/About.js
+++ src/components/About-Section/About.js
@@ -18,7 +18,7 @@ function About() {
     <div className='about-text'>
         <h1 data-aos='fade-right' >About Me</h1>
         <p id='about-para' data-aos='fade-left'>
-  I am a Full-Stack Web Developer born in Sandefjord, Norway, but currently living in Salt Lake City, UT. As a recent bootcamp graduate i am frequently looking for new projects and people to collaborate with. When i'm not coding im probably skateboarding.
+I am a front-end-web developer with a passion for creating beautiful, usable, and professional websites using best practices and a mobile first approach. When starting a new project i like to define what components i will need for certain parts of the website. For layout designs, i utilize several tools such as, Adobe XD, Adobe Illustrator, and Dreamweaver. For the actual code implementation i take a component based approach and utilize React aswell as several other react libraries that vary, depending on the task at hand. When im not busy working on projects, i love being outside and spending time at my local skatepark.
             </p>
             <br/>
         

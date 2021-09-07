5c100e31fd805147e55ea01d3ac03c65a393f194
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2021-09-07 12:58:42 -0600

    updated resume lin

--- src/components/ResumeBtn/Resume.js
+++ src/components/ResumeBtn/Resume.js
@@ -4,7 +4,7 @@ import './Resume.css'
 function Resume() {
     return (
         <div className='resume-btn'>
-            <button type='button'><a rel="noopener noreferrer" href='https://drive.google.com/file/d/17zRDG8RHvMqohRWCveToqYLXtKkrmGqK/view?usp=sharing' target='_blank'>Download My Resume!</a></button>
+            <button type='button'><a rel="noopener noreferrer" href='https://docs.google.com/document/d/10mrOwwFZ1Y_E8jsc7CQsJBNR-bPDT8q2mxe8kI3LqLg/edit?usp=sharing' target='_blank'>Download My Resume!</a></button>
         </div>
     )
 }
